45a3f8e22a92605a4b96d87c505fe6d05ea322ab
Zoltán Balogh <zoltan@bakter.hu>
2013-11-20 09:38:41 +0200

    Fixing #1252965: Security policy groups are not saved

--- src/ubuntu/ubuntupackagingwidget.cpp
+++ src/ubuntu/ubuntupackagingwidget.cpp
@@ -204,7 +204,6 @@ void UbuntuPackagingWidget::save(bool bSaveSimple) {
         }
         m_apparmor.setPolicyGroups(m_projectName,items);
         m_manifest.save();
-        m_apparmor.setRaw(ui->plainTextEditAppArmorJson->toPlainText());
         m_apparmor.save();
         save_excludes();
 
