5bcf39ad105acf0936e65fec7aa4c8504aab221d
Alexandre Abreu <alexandre.abreu@canonical.com>
2013-12-05 12:52:02 -0500

    remove simple html5 example & update project type

--- share/qtcreator/templates/wizards/ubuntu/html5-simple/html/app.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    var UI = new UbuntuUI();
-    UI.init();
-
-    UI.button('tapme').click(function () {
-	UI.element('#mytext').innerText = '..world!';
-    });
-};
-
