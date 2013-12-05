aa56676b1eaab024e01824df518badbc1cd8141f
Zoltán Balogh <zoltan@bakter.hu>
2013-12-05 10:26:46 +0100

    the qt4project is renamed to qmakeproject in QtC3.0

--- src/ubuntu/ubuntuprojectapplicationwizard.h
+++ src/ubuntu/ubuntuprojectapplicationwizard.h
@@ -21,7 +21,7 @@
 
 #include <coreplugin/basefilewizard.h>
 #include <projectexplorer/baseprojectwizarddialog.h>
-#include <qmakeprojectmanager/qt4project.h>
+#include <qmakeprojectmanager/qmakeproject.h>
 #include <qmlprojectmanager/qmlproject.h>
 #include <extensionsystem/pluginmanager.h>
 
