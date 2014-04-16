6c4349aefd141b38810c93a176fe4604aa44a336
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2014-04-16 12:29:56 +0300

    Minor tweaks to scope preview template.

--- share/qtcreator/templates/wizards/ubuntu/scope/src/displayName-preview.cpp
+++ share/qtcreator/templates/wizards/ubuntu/scope/src/displayName-preview.cpp
@@ -21,28 +21,28 @@ void %DISPLAYNAME_CAPITAL%Preview::cancelled()
 void %DISPLAYNAME_CAPITAL%Preview::run(unity::scopes::PreviewReplyProxy const& reply)
 {
     PreviewWidgetList widgets;
-    widgets.emplace_back(PreviewWidget(R"({"id": "header", "type": "header", "title": "title", "subtitle": "author", "rating": "rating"})"));
-    widgets.emplace_back(PreviewWidget(R"({"id": "img", "type": "image", "art": "screenshot-url"})"));
+    widgets.emplace_back(PreviewWidget(R"({"id": "header", "type": "header", "components" : { "title": "title", "subtitle": "author" } })"));
+    widgets.emplace_back(PreviewWidget(R"({"id": "img", "type": "image", "components" : { "source": "screenshot-url" } })"));
 
     PreviewWidget w("img2", "image");
     w.add_attribute_value("zoomable", Variant(false));
-    w.add_attribute_mapping("art", "screenshot-url");
+    w.add_attribute_mapping("source", "screenshot-url");
     widgets.emplace_back(w);
 
     ColumnLayout layout1col(1);
-    layout1col.add_column({"header", "title"});
+    layout1col.add_column({"header", "img", "img2"});
 
     ColumnLayout layout2col(2);
-    layout2col.add_column({"header", "title"});
-    layout2col.add_column({"author", "rating"});
+    layout2col.add_column({"header", "img"});
+    layout2col.add_column({"img2"});
 
     ColumnLayout layout3col(3);
-    layout3col.add_column({"header", "title"});
-    layout3col.add_column({"author"});
-    layout3col.add_column({"rating"});
+    layout3col.add_column({"header"});
+    layout3col.add_column({"img"});
+    layout3col.add_column({"img2"});
 
     reply->register_layout({layout1col, layout2col, layout3col});
     reply->push(widgets);
     reply->push("author", Variant("Foo"));
-    reply->push("rating", Variant("4 blah"));
+    reply->push("screenshot-url", Variant("/path/to/image.png"));
 }
