efe924aac4a54fdf8c05a0ffc9eab0269e3e1949
Alexandre Abreu <alexandre.abreu@canonical.com>
2015-04-09 13:58:30 -0400

    tweak grunt files

--- share/qtcreator/templates/wizards/ubuntu/html5-simple/www/js/ui.js
+++ share/qtcreator/templates/wizards/ubuntu/html5-simple/www/js/ui.js
@@ -1,20 +1,6 @@
 /**
  * Wait before the DOM has been loaded before initializing the Ubuntu UI layer
  */
-function Application(UIContext) {
-    this._uiContextClass = UIContext;
-    this._initialized = false;
-};
-Application.prototype.init = function() {
-    if (this._uiContextClass && !this._initialized) {
-        this._initialized = true;
-        var UI = new this._uiContextClass();
-        UI.init();
-    }
-};
-Application.prototype.initialized = function() {
-    return this._initialized;
-};
 
 window.onload = function () {
     function addClass(elem, className) {
